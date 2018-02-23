6b0b42732969fcee605879a3f0af2ed6ee039049
Pablo Argote <pfa2111@columbia.edu>
2018-02-23 13:59:35 -0500

    commiting files

---
+++ test.R
@@ -0,0 +1,5 @@
+# Exercise 4.10 -- Kansas City Clustered Design
+
+rm(list=ls())       # clear objects in memory
+library(ri)         # load the RI package
+set.seed(1183)   # random number seed, so that results are reproducible
